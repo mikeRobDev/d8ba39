5192b46fd06ab88f1f60f24e5ea70b9f2293b447
mikeRobDev <100727585+mikeRobDev@users.noreply.github.com>
2022-03-08 04:53:12 -0500

    Update db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,14 +1,6 @@
 const Sequelize = require("sequelize");
 
-/*const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
-});*/
-
-//change to environment variable once windows error solution is found
-const db = new Sequelize("messenger", "postgres", "HatchTrack22!", {
-  host: "localhost",
-  port: "5432",
-  dialect: 'postgres',
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
   logging: false
 });
 
